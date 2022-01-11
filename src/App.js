e6915f3b94bbb624eb1fcd5f852118d691432ded
Rakib Ahmed <rakib987@gmail.com>
2022-01-11 18:55:15 -0500

    initialize React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sass/App.css';
+// import $ from 'jquery';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        React Netflix Clone
+      </div>
+    );  
+  }
+}
