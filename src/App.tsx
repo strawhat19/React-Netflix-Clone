2cdb112ce3b553b229ea11fcdcc80e04c23797e4
Rakib Ahmed <rakib987@gmail.com>
2022-01-11 21:10:29 -0500

    getting movies in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import Main from './components/Main/main';
+import Header from './components/Header/header';
 import './sass/App.css';
 // import $ from 'jquery';
 
 export default class App extends React.Component {
+  componentDidMount(): void {
+    // console.clear();
+
+    const APIKey = `da9b0d504005e1243db4e403678fba18`;
+    const requests = {
+      trending: `hello`
+    }
+
+    const getMovie = async () => {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${APIKey}`);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+
+    getMovie();
+  }
   render() {
     return (
       <div className="App">
-        React Netflix Clone
-        <Main />
+        <Header />
       </div>
     );  
   }
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/header.css';
+const Header: React.FC = () => {
+    return (
+        <header>
+            This is the Header section, testing TypeScript
+                <br />
+            Making a React Netflix Clone
+        </header>
+    )
+}
+
+export default Header
